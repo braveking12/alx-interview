bf39fcc96d329e391e6572c69a76b38d122c2f26
root <davidkadofficial@gmail.com git config --global user.email davidkadofficial@gmail.com>
2024-05-23 14:50:35 +0000

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,42 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(url, async function (error, response, body) {
-  if (error) {
-    return console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character in characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(characters[character], (err, res, html) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(html).name);
-          }
-        });
-      });
-      console.log(res);
-    }
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
+
+  if (res.statusCode !== 200) {
+    console.error(`Failed to get movie data: ${res.statusCode}`);
+    process.exit(1);
+  }
+
+  const characterUrls = body.characters;
+
+  characterUrls.forEach((characterUrl) => {
+    request(characterUrl, { json: true }, (err, res, body) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      if (res.statusCode !== 200) {
+        console.error(`Failed to get character data: ${res.statusCode}`);
+        process.exit(1);
+      }
+
+      console.log(body.name);
+    });
+  });
 });
+
